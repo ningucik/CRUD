ccdf1f5e92fd43fb6a7698c3d9f4fb17aeed85e8
ningucik <nengucik8432@gmail.com>
2022-07-02 18:14:12 +0100

    crud

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+        <a class="btn btn-success" href="/mahasiswa/create">Add Mahasiswa</a>
+    <table class="table table-success table-striped">
+        <tr>
+            <th>NO</th>
+            <th>NAMA</th>
+            <th>NIM</th>
+            <th>PRODI</th>
+            <th>JENIS KELAMIN</th>
+            <th>ALAMAT</th>
+            <th>AKSI</th>
+        </tr>
+        @foreach($mahasiswa as $m)
+            <tr>
+                <td>{{$loop->iteration}}</td>
+                <td>{{$m->nama}}</td>
+                <td>{{$m->nim}}</td>
+                <td>{{$m->prodi}}</td>
+                <td>{{$m->jenis_kelamin}}</td>
+                <td>{{$m->alamat}}</td>
+                <td>
+                     <div class="btn-group" role="group" aria-label="Basic example">
+                        <a class="btn btn-warning" href="/mahasiswa/{{$m->id}}/edit">Edit</a>
+                        <form action="/mahasiswa/{{$m->id}}" method="POST">
+                         @csrf
+                            @method('delete')
+                            <input class="btn btn-danger" type="submit" value="Delete">
+                        </form>
+                    </div> 
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    </div>
+@endsection
+    
+   
+
+
